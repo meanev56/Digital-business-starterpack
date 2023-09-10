fe4419f13d4084a376c1be365ee9005452e31e00
meanev56 <emekanjoku86@gmail.com>
2023-09-10 13:54:36 +0100

    containert

--- src/components/BrandingVideo/BrandingVideo.jsx
+++ src/components/BrandingVideo/BrandingVideo.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+'use client'
+
+import React, { useRef } from 'react'
+import './BrandingVideo.css'
+import { motion, useScroll, useTransform } from 'framer-motion'
 
 const BrandingVideo = () => {
+    const ref = useRef(null)
+
+    const {scrollYProgress} = useScroll({
+        target: ref,
+        offset: ['start end', 'end, end']
+    })
+
+    const scale = useTransform(scrollYProgress, [0, 1], [.6, 1])
+
   return (
-    <div>
-      
+    <div className='bv-container'>
+      <div className="container">
+        <motion.video
+            className='bv-video'
+            ref={ref}
+            loop 
+            muted 
+            autoPlay 
+            controls=""
+          style={{scale}}
+        >
+            <source src='/video.mp4' type='video/mp4'/>
+        </motion.video>
+      </div>
     </div>
   )
 }
---
+++ src/components/WhatWeDo/WhatWeDo.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './WhatWeDo.css'
+
+const WhatWeDo = () => {
+  return (
+    <div className='wwd-wrapper'>
+      <div className="container">
+        <div className="wwd-container">
+            <div className="wwd-head">
+                <span className='tag'>What we do</span>
+                <span className='title'>Empowering with non dilutive capital and excution expertise</span>
+                <span className="des"> Here is how we can evaluate</span>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default WhatWeDo
